4a73500d89d07dc99c470b77f619ce90ccf360ff
matheusfillipe <matheusfillipeag@gmail.com>
2021-04-16 16:21:03 -0300

    build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+[[ $1 == "install" ]] && python setup.py install --user 
+[[ $1 == "update" ]] && rm -r dist; rm -r re_ircbot.egg-info; python3 setup.py sdist bdist_wheel; python3 -m twine upload dist/* --verbose
